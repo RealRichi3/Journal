a45f8df394e48edd7e81a5abb637fee9eb4995b5
RealRichi3 <molunorichie@gmail.com>
2022-05-27 20:54:47 +0000

    Added routes for HTTP requests and response

---
+++ server-side/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+const userController = require('../controllers/userController')
+
+router.get('/', userController.usersIndex)
+router.get('/finduser', userController.findUser)
+router.post('/adduser', userController.addUser)
+router.post('/update', userController.updateUserDetails)
+router.post('/delete', userController.deleteUser)
+
+module.exports = router
