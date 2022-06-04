90debca427452c58e3e712b7a3fc5c0f94d119d8
RealRichi3 <Molunorichie@gmail.com>
2022-06-04 04:21:08 +0100

    Added HTTPrequest handler and click action response for Submit (Signup button)

--- login-page/loginSignup.js
+++ login-page/loginSignup.js
@@ -1,5 +1,6 @@
-let createAccount = document.getElementsByClassName('createAcc')[0];
-let createAccForm = document.getElementsByClassName('overlayS')[0];
+const createAccount = document.getElementsByClassName('createAcc')[0];
+const createAccForm = document.getElementsByClassName('overlayS')[0];
+const signUpBtn = document.getElementsByClassName('signUpBtn')[0];
 const close = document.getElementsByClassName('close')[0]
 
 // Action: Create account link
@@ -12,3 +13,54 @@ createAccount.addEventListener("click", function(){
 close.addEventListener('click', () => {
     createAccForm.style.visibility = 'hidden'
 })
+
+
+// REQUESTS
+function sendHttpRequest (method, url, data) {
+    const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url); 
+        xhr.responseType = 'json'
+        
+        if (data){
+            xhr.setRequestHeader('Content-Type', 'application/json')
+        }
+
+        xhr.onload = () => {
+            if (xhr.status == 200) {
+                resolve(xhr.response)
+            }
+        }
+        xhr.onerror = () => {
+            reject('An error occured!')
+        }
+
+        xhr.send(JSON.stringify(data));
+    })
+    return promise
+}
+
+function matchPassword (password, confirmPassword) {
+    if (password != confirmPassword){
+        return False
+    }
+}
+
+signUpBtn.addEventListener('click', () => {
+    const name = document.getElementById('fname')
+    const email = document.getElementById('email')
+    const password = document.getElementById('password')
+    const confirmPassword = document.getElementById('confirmpassword')
+
+    password
+    let url = 'http://localhost:5000/user/adduser'
+    sendHttpRequest('POST', url, {
+        "name": name,
+        "email": email,
+        "password": password
+    }).then(response => {
+        console.log(response.message)
+    }, error => {
+        console.log(error.message)
+    })
+})           
