a4e9a7c10a23a77972c159d29b807fda7fe84ff7
RealRichi3 <Molunorichie@gmail.com>
2022-06-04 22:02:09 +0100

    Added popUp message response for signup action

--- login-page/loginSignup.js
+++ login-page/loginSignup.js
@@ -1,116 +1,162 @@
-const createAccount = document.getElementsByClassName('createAcc')[0];
-const createAccForm = document.getElementsByClassName('overlayS')[0];
-const signUpBtn = document.getElementsByClassName('signUpBtn')[0];
-const close = document.getElementsByClassName('close')[0]
+const createAccount = document.getElementsByClassName("createAcc")[0];
+const createAccForm = document.getElementsByClassName("overlayS")[0];
+const signUpBtn = document.getElementsByClassName("signUpBtn")[0];
+const close = document.getElementsByClassName("close")[0];
 
 // Action: Create account link
-createAccount.addEventListener("click", function(){
-    console.log('in')
-	createAccForm.style.visibility = 'visible'
+createAccount.addEventListener("click", function () {
+    createAccForm.style.visibility = "visible";
 });
 
 // Close overlay for Signup
-close.addEventListener('click', () => {
-    createAccForm.style.visibility = 'hidden'
-})
-
+close.addEventListener("click", () => {
+    createAccForm.style.visibility = "hidden";
+});
 
 // REQUESTS
-function sendHttpRequest (method, url, data) {
+function sendHttpRequest(method, url, data) {
     const promise = new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
-        xhr.open(method, url); 
-        xhr.responseType = 'json'
-        
-        if (data){
-            xhr.setRequestHeader('Content-Type', 'application/json')
+        xhr.open(method, url);
+        xhr.responseType = "json";
+
+        if (data) {
+            xhr.setRequestHeader("Content-Type", "application/json");
         }
 
         xhr.onload = () => {
             if (xhr.status == 200) {
-                resolve(xhr.response)
+                resolve(xhr.response);
             }
-        }
+        };
         xhr.onerror = () => {
-            reject('An error occured!')
-        }
+            reject("An error occured!");
+        };
 
         xhr.send(JSON.stringify(data));
-    })
-    return promise
+    });
+    return promise;
 }
 
-function checkInputs (name, email, password, confirmPassword) {
-    let [checkResultName, checkResultEmail, checkResultPassword] = [true, true, true]
-    
-    // Check name length
+// Checks if User details match Criteria
+function checkInputs(name, email, password, confirmPassword) {
+    let [checkResultName, checkResultEmail, checkResultPassword] = [
+        true,
+        true,
+        true
+    ];
+
+    // Checks name length
     {
-        let nameTextWarning = document.getElementsByClassName('cName')[0]
-        if (name.length <= 3){
-            nameTextWarning.innerHTML = "Full name must have 3 or more characters" 
-            checkResultName = false
-        }else { 
-            nameTextWarning.innerHTML = ''
-            console.log('Name is valid') }
-    }
-    
-    // Check email format
-    let emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    let emailWarningMsg = document.getElementsByClassName('cEmail')[0]
-    if (email.match(emailFormat)) { emailWarningMsg.innerHTML = ''}
-    else {
-        emailWarningMsg.innerHTML = "Invalid E-mail format"
-        checkResultEmail = false
+        let nameTextWarning = document.getElementsByClassName("cName")[0];
+        if (name.length <= 3) {
+            nameTextWarning.innerHTML =
+                "Full name must have 3 or more characters";
+            checkResultName = false;
+        } else {
+            nameTextWarning.innerHTML = "";
+        }
     }
 
+    // Checks email format
+    {
+        let emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        let emailWarningMsg = document.getElementsByClassName("cEmail")[0];
+        if (email.match(emailFormat)) {
+            emailWarningMsg.innerHTML = "";
+        } else {
+            emailWarningMsg.innerHTML = "Invalid E-mail format";
+            checkResultEmail = false;
+        }
+    }
     // Password Check
     {
-        const passwordFormat = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+        const passwordFormat =
+            /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+        let noMatchMsg = document.getElementsByClassName("cPwd")[0];
 
         // Check Password Format
         if (password.match(passwordFormat)) {
-            console.log('Password format valid')
-        }else {
-            let noMatchMsg = document.getElementsByClassName('cPwd')[0]
-            noMatchMsg.innerHTML = 'Minimun - 8 characters, must have Uppercase, lowercase, symbol and number'
-            checkResultPassword = false
+            noMatchMsg.innerHTML = "";
+        } else {
+            noMatchMsg.innerHTML =
+                "Minimun - 8 characters, must have at least 1 Uppercase, 1 lowercase, and 1 number";
+            checkResultPassword = false;
         }
-        
+
         // Check if passwords match
-        if (password == confirmPassword){
-            console.log('Passwords match')
-        }else { 
-            let passwordMatchMsg = document.getElementsByClassName('cCPwd')[0]
-            passwordMatchMsg.innerHTML = "Password doesn't match"
-            checkResultPassword = false }
+        let passwordMatchMsg = document.getElementsByClassName("cCPwd")[0];
+        if (password == confirmPassword) {
+            passwordMatchMsg.innerHTML = "";
+        } else {
+            passwordMatchMsg.innerHTML = "Password doesn't match";
+            checkResultPassword = false;
+        }
     }
-    console.log(checkResultName & checkResultEmail & checkResultPassword)
-    return (checkResultName & checkResultEmail & checkResultPassword)
+
+    return checkResultName & checkResultEmail & checkResultPassword;
 }
 
-signUpBtn.addEventListener('click', () => {
-    const name = document.getElementById('fname').value
-    const email = document.getElementById('email').value
-    const password = document.getElementById('password').value
-    const confirmPassword = document.getElementById('confirmPassword').value
-    
-    check = checkInputs(name, email, password, confirmPassword)
-    if (check){
-        let url = 'http://localhost:5000/user/adduser'
-        sendHttpRequest('POST', url, {
-            "name": name,
-            "email": email,
-            "password": password
-        }).then(response => {
-            console.log(response.message)
-            // Add siigned up successfully msg here
-        }, error => {
-            console.log(error.message)
-        })
-    }else {
-        console.log('Invalid inputs')
+let [checkResultName, checkResultEmail, checkResultPassword] = [
+    true,
+    true,
+    true
+];
+
+const [popUpBox, popUpIcon, popUpMsg] = [
+    document.getElementsByClassName("finalMsg")[0],
+    document.getElementsByClassName("msgIcon")[0],
+    document.getElementById("popUpMsg")
+];
+// Display timeout for popUp box after sending input details
+function popUp(iconPath, textContent, closeOverlay) {
+    popUpBox.style.visibility = "visible";
+    popUpIcon.setAttribute("src", `${iconPath}`);
+    popUpMsg.innerHTML = textContent;
+
+    setTimeout(
+        (close) => {
+            popUpBox.style.visibility = "hidden";
+            if (close) {
+                createAccForm.style.visibility = "hidden";
+            }
+        },
+        3000,
+        closeOverlay
+    );
+}
+
+signUpBtn.addEventListener("click", () => {
+    const name = document.getElementById("fname").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+
+    check = checkInputs(name, email, password, confirmPassword);
+    if (check == 1) {
+        let url = "http://localhost:5000/user/adduser";
+        sendHttpRequest("POST", url, {
+            name: name,
+            email: email,
+            password: password
+        }).then(
+            (response) => {
+                console.log(response.message);
+                popUp("../img/icon/successful.png", "Successfull", true);
+            },
+            (error) => {
+                console.log(error.message);
+                popUp(
+                    "../img/icon/error-occured.png",
+                    "An Error occured",
+                    true
+                );
+            }
+        );
+    } else {
+        popUp("../img/icon/invalid-details.gif", "Invalid details", false);
     }
-})           
+});
 
 // // Get the input field
 // var input = document.getElementById("myInput");
@@ -127,4 +173,21 @@ signUpBtn.addEventListener('click', () => {
 //   } else {
 //     text.style.display = "none"
 //   }
-// }); 
+// });
+
+// Links for icon files
+{
+    /* <a
+                target="_blank"
+                href="https://icons8.com/icon/zXYxiHplRLDm/error-cloud"
+                >Error Cloud</a
+            >
+            icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+            <a
+                target="_blank"
+                href="https://icons8.com/icon/rCpjcVTbx4zo/box-important"
+                >Box Important</a
+            >
+            icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+            <!-- partial --> */
+}
