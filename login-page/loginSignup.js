fdf99a6af399db4a1547b13d5baceb0341d71932
RealRichi3 <Molunorichie@gmail.com>
2022-06-07 21:55:57 +0100

    Testing signIn with google

--- login-page/loginSignup.js
+++ login-page/loginSignup.js
@@ -159,6 +159,14 @@ signUpBtn.addEventListener("click", () => {
     }
 });
 
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log("Name: " + profile.getName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
 // // Get the input field
 // var input = document.getElementById("myInput");
 
