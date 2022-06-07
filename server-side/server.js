b277f63b99c2c0f48bf9af9a96611c8851376598
RealRichi3 <Molunorichie@gmail.com>
2022-06-07 13:11:39 +0100

    Added Middleware to get githubPages request header

--- server-side/server.js
+++ server-side/server.js
@@ -30,7 +30,7 @@ app.use(morgan("dev"));
 // Set access control allow origin
 app.use(
     cors({
-        origin: ["http://127.0.0.1:5500", "http://github.io"]
+        origin: ["http://127.0.0.1:5500", "https://github.io"]
     })
 );
 
