e928c75342af7dd916606ebe11c933dfb50b39f9
RealRichi3 <Molunorichie@gmail.com>
2022-06-14 03:22:32 +0100

    Added route to confirm login details

--- server-side/routes/userRoutes.js
+++ server-side/routes/userRoutes.js
@@ -7,6 +7,7 @@ router.get("/showall", userController.usersIndex);
 // router.post("/finduser", userController.findUser);
 router.post("/matchuser", userController.findUserMatch);
 router.post("/adduser", userController.addUser);
+router.post("/login", userController.confirmLogin);
 router.post("/update", userController.updateUserDetails);
 router.post("/delete", userController.deleteUser);
 
