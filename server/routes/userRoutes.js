44b118fa7219ed0b336eab9bfc6add6042650a8e
RealRichi3 <Molunorichie@gmail.com>
2022-06-27 04:09:06 +0100

    Added routes to confirmResetToken and updateUserPassword

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,14 +3,18 @@ const router = express.Router();
 
 const userController = require("../controllers/userController");
 const confirmLogin = require("../controllers/passwordController").confirmLogin;
+const confirmResetToken =
+    require("../controllers/passwordController").confirmResetToken;
 
 router.get("/showall", userController.usersIndex);
-// router.post("/finduser", userController.findUser);
 router.post("/matchuser", userController.findUserMatch);
 router.post("/adduser", userController.addUser);
-router.post("/login", confirmLogin);
 router.post("/update", userController.updateUserDetails);
 router.post("/reset-password", userController.resetUserPassword);
+router.post("/update-password", userController.updateUserPassword);
 router.post("/delete", userController.deleteUser);
+
+router.post("/login", confirmLogin);
+router.post("/reset-password/confirm-token", confirmResetToken);
 
 module.exports = router;
